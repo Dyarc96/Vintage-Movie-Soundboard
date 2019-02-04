bc65f5db8863635c30ea5c5b96b3eab27534023f
Dyarc96 <jandyja96@hotmail.com>
2019-02-04 17:42:23 +0100

    init

--- main.js
+++ main.js
@@ -1,20 +1,27 @@
-function playSound(e){
+window.addEventListener('keydown', function(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    if(!audio) return; //stop the function from running
-    audio.currentTime = 0; //rewind to start
+    if(!audio) return;
+    audio.currentTime = 0;
     audio.play();
-    e.preventDefault();
     key.classList.add('playing');
-};
-
-
+});
+  
 function removeTransition(e) {
     if(e.propertyName !== 'transform') return;
     this.classList.remove('playing');
 }
 
+function playOnClick (e) {
+    const keyCode = this.getAttribute('data-key');
+    const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${keyCode}"]`);
+    audio.currentTime = 0;
+    key.classList.add('playing');
+    audio.play();
+}
+
+
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-window.addEventListener('keydown', playSound);
-window.addEventListener('touchstart', playSound);
+keys.forEach(key => key.addEventListener('click', playOnClick));
